19dd35b6a7a4c4b8c68ab1985d195fa51017caa0
Reda Ghardi <redaghardi5@gmail.com>
2024-04-03 17:45:42 +0100

    better

--- scrapimdb.py
+++ scrapimdb.py
@@ -1,9 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 r = requests.get('https://www.imdb.com/list/ls055592025/')
 page_contents = r.text
 doc = BeautifulSoup(page_contents, 'html.parser')
+
+
+Name=[]
+Year=[]
+Genre=[]
+Time=[]
+Ratings=[]
+Stars=[]
+Gross=[]
+Votes=[]
+OscarNomination=[]
+Oscars=[]
+# movie_data=doc.find_all('div',attrs={'class':'lister-item mode-detail'})
+
+
+
+
 "MOVIE NAMES "
 # movie_names = doc.find_all('h3', class_='lister-item-header')
 # for ele in movie_names:
@@ -14,11 +32,11 @@ doc = BeautifulSoup(page_contents, 'html.parser')
 # movies_year = doc.find_all('span', class_='lister-item-year text-muted unbold')
 # for year in movies_year:
 #     print(year.text.strip())
-# "GENRE"
+"GENRE"
 # movies_genre=doc.find_all('span', class_='genre')
 # for genre in movies_genre:
 #     print(genre.text.strip())
-# "DURATION"
+"DURATION"
 # movie_time=doc.find_all('span', class_='runtime')
 # for t in movie_time:
 #     print(t.text.strip())
@@ -43,7 +61,6 @@ doc = BeautifulSoup(page_contents, 'html.parser')
 "MOVIE STARS"
 # p_tags = doc.find_all('p', class_='text-muted text-small')
 
-# Print the actor names
 
 # for p_tag in p_tags:
 #     if 'Stars:' in p_tag.text:
@@ -52,44 +69,68 @@ doc = BeautifulSoup(page_contents, 'html.parser')
 #         stars = stars[1:]
 #         print(stars)
 
-"MOVIES WITH ONE DIRECTOR"
-# movie_director = doc.find_all('p', class_='text-muted text-small')
-# for ele in movie_director:
-#     director_tag = ele.find('a')
-#     if director_tag:
-#         print(director_tag.text)
-
- 
-   
-#  print(ele.find('p').text)
- 
-#  print("item finished")
 
 "Gross earnings"
-# span_elements = doc.find_all('span', {'name': 'nv', 'data-value': True})
-    
-
-    
+   
 # span_elements = doc.find_all('span', {'name': 'nv', 'data-value': True})
     
 # for span_element in span_elements:
 #         text_content = span_element.text
         
 #         if text_content.endswith("M"):
-#             print("Text Content:", text_content)
+#             print("Gross:", text_content)
 
 
-# p_tags = doc.find_all('p', class_='text-muted text-small')
+
 
 
 
 "MOVIES WITH MORE THEN ONE DIRECTOR "       
-p_tags = doc.find_all('p', class_='text-muted text-small')
+# p_tags = doc.find_all('p', class_='text-muted text-small')
 
-for p_tag in p_tags:
-    if ('Directors:')    in p_tag.text:
-        director_links = p_tag.find_all('a', href=lambda x: x and '/name/' in x)
-        directors = [director.text for director in director_links]
-        print(directors[:-4])
-          # Stop searching after finding directors
+# for p_tag in p_tags:
+#     if ('Directors:')    in p_tag.text:
+#         director_links = p_tag.find_all('a', href=lambda x: x and '/name/' in x)
+#         directors = [director.text for director in director_links]
+#         print(directors[:-4])
+          
 
+
+movie_data = doc.find_all('div', attrs={'class': 'lister-item mode-detail'})
+for i in movie_data:
+    name=i.h3.a.text
+    Name.append(name)
+# print(Name)
+    movie_year = i.find('span', class_='lister-item-year text-muted unbold').get_text(strip=True)
+    Year.append(movie_year)
+
+# print(years)
+    movies_genre=i.find('span', class_='genre').get_text(strip=True)
+    Genre.append(movies_genre)
+# print(Genre)    
+    movie_time=i.find('span', class_='runtime').get_text(strip=True).replace('min','')
+    Time = [int(item) for item in Time]# convert to integers 
+    Time.append(movie_time)
+# print(Time)  
+    rating=i.find('div', class_='ipl-rating-star small').get_text(strip=True)
+    Ratings = [float(item) for item in Ratings]
+    Ratings.append(rating) 
+# print(Ratings)
+### GROSS SECTION
+    value=i.findAll('span',attrs={'name':'nv'})
+    vote=value[0].text
+    Votes.append(vote)
+    grosses=value[1].text if len(value)>1 else '*****'
+    Gross.append(grosses)
+## Oscars  
+    # oscar_info = i.find(text='Oscars:')
+    # oscar_nominations = oscar_info.find_next_sibling(text='Oscar Nominations:')
+    # oscars = oscar_info.find_next_sibling().find_next_sibling().strip()
+    # Oscars.append(oscars + " (" + oscar_nominations + ")")
+    # print(Oscars)
+  
+
+
+movie_DF=pd.DataFrame({'Name Of Movie':Name , 'Release Year':Year,'Genre':Genre,'Duration In Minutes':Time,'Ratings':Ratings,'Votes':Votes,'Gross Earnings':Gross})    
+
+# print(movie_DF)
