78b6be21201596ca43ddcc9395c74ac8e42545ce
Reda Ghardi <redaghardi5@gmail.com>
2024-04-03 23:14:28 +0100

    number of win of oscar

--- scrapimdb.py
+++ scrapimdb.py
@@ -1,24 +1,24 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import json
+# import requests
+# from bs4 import BeautifulSoup
+# import pandas as pd
+# import json
 
-r = requests.get('https://www.imdb.com/list/ls055592025/')
-page_contents = r.text
-doc = BeautifulSoup(page_contents, 'html.parser')
+# r = requests.get('https://www.imdb.com/list/ls055592025/')
+# page_contents = r.text
+# doc = BeautifulSoup(page_contents, 'html.parser')
 
 
-Name=[]
-Year=[]
-Genre=[]
-Time=[]
-Ratings=[]
-Stars=[]
-Gross=[]
-Votes=[]
-OscarNominations=[]
-Prizes=[]
-OscarsWon=[]
+# Name=[]
+# Year=[]
+# Genre=[]
+# Time=[]
+# Ratings=[]
+# Stars=[]
+# Gross=[]
+# Votes=[]
+# OscarNominations=[]
+# Prizes=[]
+# OscarsWon=[]
 # movie_data=doc.find_all('div',attrs={'class':'lister-item mode-detail'})
 
 
@@ -98,43 +98,53 @@ OscarsWon=[]
           
 
 
-movie_data = doc.find_all('div', attrs={'class': 'lister-item mode-detail'})
-for i in movie_data:
-    name=i.h3.a.text
-    Name.append(name)
+# movie_data = doc.find_all('div', attrs={'class': 'lister-item mode-detail'})
+# for i in movie_data:
+#     name=i.h3.a.text
+#     Name.append(name)
 # print(Name)
-    movie_year = i.find('span', class_='lister-item-year text-muted unbold').get_text(strip=True)
-    Year.append(movie_year)
+    # movie_year = i.find('span', class_='lister-item-year text-muted unbold').get_text(strip=True)
+    # Year.append(movie_year)
 
 # print(years)
-    movies_genre=i.find('span', class_='genre').get_text(strip=True)
-    Genre.append(movies_genre)
+    # movies_genre=i.find('span', class_='genre').get_text(strip=True)
+    # Genre.append(movies_genre)
 # print(Genre)    
-    movie_time=i.find('span', class_='runtime').get_text(strip=True).replace('min','')
-    Time = [int(item) for item in Time]# convert to integers 
-    Time.append(movie_time)
+    # movie_time=i.find('span', class_='runtime').get_text(strip=True).replace('min','')
+    # Time = [int(item) for item in Time]# convert to integers 
+    # Time.append(movie_time)
 # print(Time)  
-    rating=i.find('div', class_='ipl-rating-star small').get_text(strip=True)
-    Ratings = [float(item) for item in Ratings]
-    Ratings.append(rating) 
+    # rating=i.find('div', class_='ipl-rating-star small').get_text(strip=True)
+    # Ratings = [float(item) for item in Ratings]
+    # Ratings.append(rating) 
 # print(Ratings)
 ### GROSS SECTION
-    value=i.findAll('span',attrs={'name':'nv'})
-    vote=value[0].text
-    Votes.append(vote)
-    grosses=value[1].text if len(value)>1 else '*****'
-    Gross.append(grosses)
+    # value=i.findAll('span',attrs={'name':'nv'})
+    # vote=value[0].text
+    # Votes.append(vote)
+    # grosses=value[1].text if len(value)>1 else '*****'
+    # Gross.append(grosses)
 # Prizes^
-    prize_info = i.find('div', class_='list-description').get_text(strip=True)
-    prize_lines = prize_info.split('\n')
-    for line in prize_lines:
-        if "Oscar Nominations" in line:
-            nominations = int(line.split(':')[1].strip())
-            OscarNominations.append(nominations)
-            break
-    else:
-        OscarNominations.append(None)
-# print(OscarNominations)        
+    # prize_info = i.find('div', class_='list-description').get_text(strip=True)
+    # prize_lines = prize_info.split('\n')
+    # for line in prize_lines:
+    #     if "Oscar Nominations" in line:
+    #         nominations = int(line.split(':')[1].strip())
+    #         OscarNominations.append(nominations)
+    #         break
+    # else:
+    #     OscarNominations.append(None)
+# print(OscarNominations)
+    # for line in prize_lines:
+    #     if "Oscars" in line:
+    #         won = int(line.split(':')[1].strip())
+    #         OscarsWon.append(won)
+    #         break
+    # else:
+    #     OscarsWon.append(None)
+# print(OscarsWon)        
+    
+        
     
     
     
@@ -145,8 +155,74 @@ for i in movie_data:
   
 
 
-movie_DF=pd.DataFrame({'Name Of Movie':Name , 'Release Year':Year,'Genre':Genre,'Duration In Minutes':Time,'Ratings':Ratings,'Votes':Votes,'Gross Earnings':Gross,'Oscar Nominations':OscarNominations})    
+# movie_DF=pd.DataFrame({'Name Of Movie':Name , 'Release Year':Year,'Genre':Genre,'Duration In Minutes':Time,'Ratings':Ratings,'Votes':Votes,'Gross Earnings':Gross,'Oscar Nominations':OscarNominations})    
 
 # print(movie_DF)
 
 
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+r = requests.get('https://www.imdb.com/list/ls055592025/')
+page_contents = r.text
+doc = BeautifulSoup(page_contents, 'html.parser')
+
+Name = []
+Year = []
+Genre = []
+Time = []
+Ratings = []
+Votes = []
+Gross = []
+OscarNominations = []
+OscarsWon = []
+
+movie_data = doc.find_all('div', attrs={'class': 'lister-item mode-detail'})
+for i in movie_data:
+    name = i.h3.a.text
+    Name.append(name)
+
+    movie_year = i.find('span', class_='lister-item-year text-muted unbold').get_text(strip=True)
+    Year.append(movie_year)
+
+    movies_genre = i.find('span', class_='genre').get_text(strip=True)
+    Genre.append(movies_genre)
+
+    movie_time = i.find('span', class_='runtime').get_text(strip=True).replace('min', '')
+    Time.append(int(movie_time))
+
+    rating = i.find('div', class_='ipl-rating-star small').get_text(strip=True)
+    Ratings.append(float(rating))
+
+    value = i.findAll('span', attrs={'name': 'nv'})
+    vote = value[0].text
+    Votes.append(vote)
+    grosses = value[1].text if len(value) > 1 else '*****'
+    Gross.append(grosses)
+
+    # Extracting Oscar Nominations
+    prize_info = i.find('div', class_='list-description').get_text(strip=True)
+    prize_lines = prize_info.split('\n')
+    for line in prize_lines:
+        if "Oscar Nominations" in line:
+            nominations = int(line.split(':')[1].strip())
+            OscarNominations.append(nominations)
+            break
+    else:
+        OscarNominations.append(None)
+    # Extracting Oscar winns
+    for line in prize_lines:
+        if "Oscars" in line:
+            won = int(line.split(':')[2].strip())
+            OscarsWon.append(won)
+            break
+    else:
+        OscarsWon.append(None)
+print(OscarsWon)        
+
+
+movie_DF = pd.DataFrame({'Name Of Movie': Name, 'Release Year': Year, 'Genre': Genre, 'Duration In Minutes': Time,
+                         'Ratings': Ratings, 'Votes': Votes, 'Gross Earnings': Gross,
+                         'Oscar Nominations': OscarNominations, 'Oscars Won': OscarsWon})
+# print(movie_DF)
